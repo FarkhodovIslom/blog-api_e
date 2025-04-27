8ed0a3294ea89d8b43e8f09d8e3a9a9cd0978ceb
FarkhodovIslom <111051883+FarkhodovIslom@users.noreply.github.com>
2025-04-27 21:19:30 +0500

    `Update post routes to include next function in controller calls`

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -4,33 +4,32 @@ import { authMiddleware } from '../middleware/auth';
 
 const router = Router();
 
-// Маршруты для постов
 router.post('/', authMiddleware, (req: Request, res: Response, next: NextFunction): void => {
-    PostController.createPost(req, res);
+    PostController.createPost(req, res, next);
 });
 
 router.get('/', (req: Request, res: Response, next: NextFunction): void => {
-    PostController.getAllPosts(req, res);
+    PostController.getAllPosts(req, res, next);
 });
 
 router.get('/sort', (req: Request, res: Response, next: NextFunction): void => {
-    PostController.sortPostsByDate(req, res);
+    PostController.sortPostsByDate(req, res, next);
 });
 
 router.get('/:id', (req: Request, res: Response, next: NextFunction): void => {
-    PostController.getPostById(req, res);
+    PostController.getPostById(req, res, next);
 });
 
 router.put('/:id', authMiddleware, (req: Request, res: Response, next: NextFunction): void => {
-    PostController.updatePost(req, res);
+    PostController.updatePost(req, res, next);
 });
 
 router.delete('/:id', authMiddleware, (req: Request, res: Response, next: NextFunction): void => {
-    PostController.deletePost(req, res);
+    PostController.deletePost(req, res, next);
 });
 
 router.get('/:postId/comments', (req: Request, res: Response, next: NextFunction): void => {
-    PostController.getComments(req, res);
+    PostController.getComments(req, res, next);
 });
 
 export default router;
